346469aa8fe90b8c394849452ac8ce0053f4d018
NonamedBread <lgh778923@gmail.com>
2024-08-11 15:16:54 +0900

    feat: Add program to find the minimum value among three numbers

--- algorithm.js
+++ algorithm.js
@@ -1,16 +1,15 @@
-const rl = require("readline").createInterface({
+const readline = require('readline')
+
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+})
 
-const input = [];
+const input = []
 
-rl.on("line", (line) => {
-  line.split(" ").forEach((el) => input.push(Number(el)));
-}).on("close", () => {
-  let temp = input[0];
-
-  for (let i = 1; i < input.length; i++) {
-    temp = temp < input[i] ? temp : input[i];
-  }
-});
+rl.on('line', line => {
+  input.push(...line.split(' ').map(Number))
+}).on('close', () => {
+  const min = Math.min(...input)
+  console.log(min)
+})
