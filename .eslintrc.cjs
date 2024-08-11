c5ee103b7423e2f27e99893727fd8ba89bb9372e
NonamedBread <lgh778923@gmail.com>
2024-08-11 15:16:45 +0900

    chore: Update ESLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,17 +3,12 @@ module.exports = {
   extends: ['eslint:recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  // rules: {
-  //   'import/order': [
-  //     'error',
-  //     {
-  //       groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-  //       'newlines-between': 'always',
-  //       alphabetize: {
-  //         order: 'asc',
-  //         caseInsensitive: true,
-  //       },
-  //     },
-  //   ],
-  // },
+  env: {
+    node: true, // Node.js 환경 추가
+    es2021: true,
+  },
+  rules: {
+    'no-console': 'off', // console.log 사용 허용
+    // 추가적인 규칙을 여기에 추가할 수 있습니다.
+  },
 }
